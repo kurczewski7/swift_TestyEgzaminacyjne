dec5d1ca84a540dd113c832e70d6d5f4cebed15a
kurczewski7 <slawomir.kurczewski@gmail.com>
2017-02-28 17:46:06 +0100

    Pierwszy commit

---
+++ TestyEgzaminacyjne/MyCustomControl.swift
@@ -0,0 +1,27 @@
+//
+//  MyCustomControl.swift
+//  TestyEgzaminacyjne
+//
+//  Created by Slawek Kurczewski on 27.02.2017.
+//  Copyright © 2017 Slawomir Kurczewski. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomControl: UIView {
+    
+    //let colorsWithTitles: [(UIColor, String)]   =
+    //let b=UIButton(frame: //)
+    //let i=UIImage
+    //let l=UILabel(frame: <#T##CGRect#>)
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
